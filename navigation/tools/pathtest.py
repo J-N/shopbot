491f825310a89384b86280e1a315028211fe7748
root <root@turtlebot.(none)>
2013-05-06 10:04:18 -0400

    latest path update

--- navigation/tools/pathtest.py
+++ navigation/tools/pathtest.py
@@ -1,11 +1,26 @@
 import time
 import urllib2
-url='http://kinetic.hp.af.cm/shopbot_list.php'
-req = urllib2.Request(url)
-res = urllib2.urlopen(req)
-str = res.read()
-str2 = str[30:]
-st3 = st2
+import sys
+
+dic = {'1':42, '2':23, '3':44, '4':23}
+while 1:
+	url='http://kinetic.hp.af.cm/shopbot_list.php'
+	req = urllib2.Request(url)
+	res = urllib2.urlopen(req)
+	raw = res.read()
+	str2 = raw[30:]
+	pos = str2.find("</body")
+	str3 = str2[:pos]
+	str3=str3.replace("<br/>"," ")
+	items=str3.split()
+	if len(items) == 0:
+		continue
+	print "New order detected:"
+	for i in xrange(0,len(items),2):
+		quant=items[i]
+		item=items[i+1]
+		print "item: " + item + " translated itemid: "+str(dic[item]) + " quanitiy " +quant
+	sys.exit(0)
 
 f=open("/dev/pathComms", "wb")
 while 1:
@@ -14,4 +29,5 @@ while 1:
 	f.write("%s" % item)
 	f.write("|")
 	f.write("%s\n" % quant)
+	f.write("H\n")
 	f.flush()
